3aae579098e8f1a020e999443c23f3ce29e8e185
Nivek <43946350+NivekUvNart@users.noreply.github.com>
2021-11-02 21:34:21 -0700

    created first file

---
+++ avildawn.cpp
@@ -0,0 +1,15 @@
+// Project Name: Avil Dawn
+// Date Started: 11/2/2021
+// Developers: Kevin Tran
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Start of project" << endl;
+
+    system("pause");
+
+    return 0;
+}
