368790de8a4b6453c1e28f8901fe77d9ba8ba953
Nivek <43946350+NivekUvNart@users.noreply.github.com>
2021-11-04 01:55:42 -0700

    create window, draw texture

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,50 @@
 // mingw32-make -f Makefile
 
 #include <SDL.h>
+#include <SDL_image.h>
 #include <iostream>
+
+#include "RenderWindow.hpp"
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main(int argv, char* args[])
 {
+    if (SDL_Init(SDL_INIT_VIDEO) > 0)
+        cout << "SDL_Init has failed. Error: " << SDL_GetError() << endl;
+
+    if (!(IMG_Init(IMG_INIT_PNG)))
+        cout << "IMG_Init has failed. Error: " << SDL_GetError() << endl;
+
+    // opens the game window
+    RenderWindow window("GAME v1.0", 1280, 720);
+
+    SDL_Texture* grassTexture = window.loadTexture("res/img/grass_1.png");
+
+    bool gameRunning = true;
+
+    SDL_Event event;
+
+    while (gameRunning == true)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                gameRunning = false;
+        }
+
+        window.clear();
+        window.render(grassTexture);
+        window.display();
+    }
+
+
+
+
+
+
+    window.cleanUp();
+    SDL_Quit();
+
     return 0;
 }
